4519ee4f09c940fdab4ef8600fab180ca92ad4f6
Yokhry <prathambaliyan012@gmail.com>
2024-02-22 09:26:31 +0530

    initial commit

---
+++ src/main/java/com/example/springbootjavaapp/SpringbootJavaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjavaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJavaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJavaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootjavaapp/SpringbootJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjavaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
